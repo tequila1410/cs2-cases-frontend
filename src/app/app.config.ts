55b8cde356043716f5daa92836493b3a8e07c9ee
Vlado <vprokhorov.work@gmail.com>
2025-01-07 03:06:08 +0200

    add inteerceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,15 @@ import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {domainInterceptor} from '@shared/interceptors/domain.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([domainInterceptor])),
     providePrimeNG({
       theme: {
         preset: Aura
