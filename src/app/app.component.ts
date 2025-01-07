1eac0669f767a4ad5d88b6a1a7dbca35be120a9d
Rio <brauholland@gmail.com>
2025-01-07 06:47:20 +0200

    init footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from '@shared/header/header.component';
+import { FooterComponent } from '@shared/footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'cs2-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
